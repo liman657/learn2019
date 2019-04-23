bb532ee08ecf7d8af890c9ac441c8b264f327360
liman657 <657271181@qq.com>
2019-04-23 23:47:58 +0800

    spring mvc手写优化

--- spring_demo/src/main/java/com/learn/springmvc/servlet/V2/SelfServletV2.java
+++ spring_demo/src/main/java/com/learn/springmvc/servlet/V2/SelfServletV2.java
@@ -1,14 +1,16 @@
-package com.learn.springmvc.servlet;
+package com.learn.springmvc.servlet.V2;
 
 import com.learn.springmvc.Annotation.*;
 
-import javax.servlet.*;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
@@ -19,7 +21,7 @@ import java.util.*;
  * autor:liman
  * comment: 自己的servlet
  */
-public class SelfServlet extends HttpServlet {
+public class SelfServletV2 extends HttpServlet {
 
     //用于保存application.properties配置文件中的内容，这个实例使用properties文件代替xml文件
     private Properties contextConfig = new Properties();
@@ -28,10 +30,12 @@ public class SelfServlet extends HttpServlet {
     private List<String> classNames = new ArrayList<String>();
 
     //传说中的IOC容器，为了简化程序，这里不用ConcurrentHashMap
-    private Map<String,Object> ioc = new HashMap<String,Object>();
+    private Map<String, Object> ioc = new HashMap<String, Object>();
 
     //保存url和Method的对应关系
-    private Map<String,Method> handlerMapping = new HashMap<String,Method>();
+    private Map<String, Method> handlerMapping = new HashMap<String, Method>();
+
+    private List<HandlerMapping>
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -42,16 +46,16 @@ public class SelfServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //6.调用
         try {
-            doDispatch(req,resp);
+            doDispatch(req, resp);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
 
-
     /**
      * 初始化方法
+     *
      * @param config
      * @throws ServletException
      */
@@ -74,64 +78,74 @@ public class SelfServlet extends HttpServlet {
 
     /**
      * 开始调用自己写的mvc框架
+     *
      * @param req
      * @param resp
      */
     private void doDispatch(HttpServletRequest req, HttpServletResponse resp) throws Exception {
 
         //获取绝对路径
-        String  url = req.getRequestURI();
+        String url = req.getRequestURI();
         System.out.println(url);
         String contextPath = req.getContextPath();
         System.out.println(contextPath);
         //处理成相对路径
-        url = url.replaceAll(contextPath,"").replaceAll("/+","/");
+        url = url.replaceAll(contextPath, "").replaceAll("/+", "/");
 
-        if(!this.handlerMapping.containsKey(url)){
+        if (!this.handlerMapping.containsKey(url)) {
             resp.getWriter().write("404 not found!!!");
             return;
         }
 
         Method method = this.handlerMapping.get(url);
 
-        System.out.println("===================="+method.getParameters());
+        System.out.println("====================" + method.getParameters());
 
         //调用方法需要对象和参数，
         //1.从request中获取参数
-        Map<String,String[]> params = req.getParameterMap();
+        Map<String, String[]> params = req.getParameterMap();
 
         //获取参数的类型列表
-        Class<?> [] parameterTypes = method.getParameterTypes();
+        Class<?>[] parameterTypes = method.getParameterTypes();
         Parameter[] parameters = method.getParameters();
 
         //存放参数的值
         Object[] paramValues = new Object[parameterTypes.length];
 
-        for(int i = 0;i<parameterTypes.length;i++){
+        for (int i = 0; i < parameterTypes.length; i++) {
             Class parameterType = parameterTypes[i];
-            if(parameterType == HttpServletRequest.class){
+            if (parameterType == HttpServletRequest.class) {
                 paramValues[i] = req;
                 continue;
-            }else if(parameterType == HttpServletResponse.class){
-                paramValues[i]=resp;
+            } else if (parameterType == HttpServletResponse.class) {
+                paramValues[i] = resp;
                 continue;
-            }else if(parameterType == String.class){
-                SelfRequestParam requestParam = (SelfRequestParam) parameters[i].getAnnotation(SelfRequestParam.class);
-                //原来是这样写的，这里涉及反射的内容，parameterType和parameter似乎不是一个东西
-//                SelfRequestParam requestParam = (SelfRequestParam) parameterType.getAnnotation(SelfRequestParam.class);
-                if(params.containsKey(requestParam.value())){
-                    for(Map.Entry<String,String[]> param:params.entrySet()){
-                        String value = Arrays.toString(param.getValue()).replaceAll("\\[|\\]","")
-                                .replaceAll("\\s",",");
-                        paramValues[i] = value;
+            }
+            Annotation[][] parameterAnnotations = method.getParameterAnnotations();
+            for (int j = 0; j < parameterAnnotations.length; j++) {
+                for (Annotation a : parameterAnnotations[j]) {
+                    if (a instanceof SelfRequestParam) {
+                        String paramName = ((SelfRequestParam) a).value();
+//                            if("".equals(paramName.trim())) {
+//                                paramValues[i] =
+//                            }
+                        if (params.containsKey(paramName)) {
+                            for (Map.Entry<String, String[]> param : params.entrySet()) {
+                                System.out.println(Arrays.toString(param.getValue()));
+                                String value = Arrays.toString(param.getValue()).replaceAll("\\[|\\]", "").replaceAll("\\s", "");
+                                paramValues[i] = value;
+                            }
+                        }
                     }
                 }
             }
+
+
         }
 
         //通过方法获取class，然后将首字母小写获得指定的bean
         String beanName = toLowerFirstCase(method.getDeclaringClass().getSimpleName());
-        method.invoke(ioc.get(beanName),paramValues);
+        method.invoke(ioc.get(beanName), paramValues);
 //        method.invoke(ioc.get(beanName),new Object[]{req,resp,params.get("name")[0]});
 
     }
@@ -141,33 +155,33 @@ public class SelfServlet extends HttpServlet {
      * HandlerMapping——url和method一对一的映射
      */
     private void initHandlerMapping() {
-        if(ioc.isEmpty()){
+        if (ioc.isEmpty()) {
             return;
         }
 
-        for(Map.Entry<String,Object> entry:ioc.entrySet()){
+        for (Map.Entry<String, Object> entry : ioc.entrySet()) {
             Class<?> clazz = entry.getValue().getClass();
-            if(!clazz.isAnnotationPresent(SelfController.class)){
+            if (!clazz.isAnnotationPresent(SelfController.class)) {
                 continue;
             }
 
             //获取写在类上面的RequestMapping的值
             String baseUrl = "";
-            if(clazz.isAnnotationPresent(SelfRequestMapping.class)){
+            if (clazz.isAnnotationPresent(SelfRequestMapping.class)) {
                 SelfRequestMapping requestMapping = clazz.getAnnotation(SelfRequestMapping.class);
                 baseUrl = requestMapping.value();
             }
 
             //遍历所有的public 方法
-            for(Method method:clazz.getMethods()){
-                if(!method.isAnnotationPresent(SelfRequestMapping.class)){
+            for (Method method : clazz.getMethods()) {
+                if (!method.isAnnotationPresent(SelfRequestMapping.class)) {
                     continue;
                 }
 
                 SelfRequestMapping requestMapping = method.getAnnotation(SelfRequestMapping.class);
-                String url = ("/"+baseUrl+"/"+requestMapping.value()).replaceAll("/+","/");
-                handlerMapping.put(url,method);
-                System.out.println("Mapped:"+url+","+method);
+                String url = ("/" + baseUrl + "/" + requestMapping.value()).replaceAll("/+", "/");
+                handlerMapping.put(url, method);
+                System.out.println("Mapped:" + url + "," + method);
             }
 
         }
@@ -178,23 +192,23 @@ public class SelfServlet extends HttpServlet {
      * 完成依赖注入
      */
     private void doAutowired() {
-        if(ioc.isEmpty()){
+        if (ioc.isEmpty()) {
             return;
         }
 
-        for(Map.Entry<String,Object> entry:ioc.entrySet()){
+        for (Map.Entry<String, Object> entry : ioc.entrySet()) {
             //获取本类中所有的字段
             Field[] fields = entry.getValue().getClass().getDeclaredFields();
 
-            for(Field field:fields){
-                if(!field.isAnnotationPresent(SelfAutowired.class)){
+            for (Field field : fields) {
+                if (!field.isAnnotationPresent(SelfAutowired.class)) {
                     continue;
                 }
                 //获取有@Autowired注解的属性上的注解对象
                 SelfAutowired selfAutowired = field.getAnnotation(SelfAutowired.class);
                 //获取注解的值
                 String beanName = selfAutowired.value().trim();
-                if("".equals(beanName)){
+                if ("".equals(beanName)) {
                     //如果没有自定义，默认根据类型注入
                     beanName = field.getType().getName();
                 }
@@ -204,7 +218,7 @@ public class SelfServlet extends HttpServlet {
 
                 try {
                     //设置属性的值
-                    field.set(entry.getValue(),ioc.get(beanName));
+                    field.set(entry.getValue(), ioc.get(beanName));
                 } catch (IllegalAccessException e) {
                     e.printStackTrace();
                 }
@@ -217,21 +231,23 @@ public class SelfServlet extends HttpServlet {
      * 加了注解的类才能初始化，这里只列出加了@Controller和@Service注解的类
      */
     private void doInstance() {
-        if(classNames.isEmpty()){return;}
+        if (classNames.isEmpty()) {
+            return;
+        }
 
-        try{
-            for(String className:classNames){
+        try {
+            for (String className : classNames) {
                 Class<?> clazz = Class.forName(className);
                 //如果有Controller注解
-                if(clazz.isAnnotationPresent(SelfController.class)){
+                if (clazz.isAnnotationPresent(SelfController.class)) {
                     Object instance = clazz.newInstance();
                     String beanName = toLowerFirstCase(clazz.getSimpleName());
-                    ioc.put(beanName,instance);
-                }else if(clazz.isAnnotationPresent(SelfService.class)){//如果有Service注解
+                    ioc.put(beanName, instance);
+                } else if (clazz.isAnnotationPresent(SelfService.class)) {//如果有Service注解
                     //获取自定义的beanName——@Service("test")获取其中的test
                     SelfService service = clazz.getAnnotation(SelfService.class);
                     String beanName = service.value();//获得注解的值（一般自己制定service的名称的时候）
-                    if("".equals(beanName.trim())){
+                    if ("".equals(beanName.trim())) {
                         //如果没有指定名称，service 默认首字母小写
                         beanName = toLowerFirstCase(clazz.getSimpleName());
                     }
@@ -239,39 +255,42 @@ public class SelfServlet extends HttpServlet {
 
                     Object instance = clazz.newInstance();
                     //放入IOC容器
-                    ioc.put(beanName,instance);
+                    ioc.put(beanName, instance);
 
                     //注入的时候是接口注入的方式，投机取巧，就将接口作为key,实例作为值
-                    for(Class<?> i:clazz.getInterfaces()){
-                        if(ioc.containsKey(i.getName())){//这样就限制了，一个接口只有一个实现类
-                            throw new Exception("the "+i.getName()+" is exists");
+                    for (Class<?> i : clazz.getInterfaces()) {
+                        if (ioc.containsKey(i.getName())) {//这样就限制了，一个接口只有一个实现类
+                            throw new Exception("the " + i.getName() + " is exists");
                         }
-                        ioc.put(i.getName(),instance);
+                        ioc.put(i.getName(), instance);
                     }
-                }else{
+                } else {
                     continue;
                 }
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     /**
      * 扫描出相关的类
+     *
      * @param scanPackage
      */
     private void doScanner(String scanPackage) {
 
         //主要是将包路径转换为文件路径
-        URL url = this.getClass().getClassLoader().getResource("/"+scanPackage.replaceAll("\\.","/"));
-        File classPath  = new File(url.getFile());
-        for(File file:classPath.listFiles()){
-            if(file.isDirectory()){
-                doScanner(scanPackage+"."+file.getName());
-            }else{
-                if(!file.getName().endsWith(".class")){continue;}
-                String className = (scanPackage+"."+file.getName().replace(".class",""));
+        URL url = this.getClass().getClassLoader().getResource("/" + scanPackage.replaceAll("\\.", "/"));
+        File classPath = new File(url.getFile());
+        for (File file : classPath.listFiles()) {
+            if (file.isDirectory()) {
+                doScanner(scanPackage + "." + file.getName());
+            } else {
+                if (!file.getName().endsWith(".class")) {
+                    continue;
+                }
+                String className = (scanPackage + "." + file.getName().replace(".class", ""));
                 classNames.add(className);
             }
         }
@@ -288,8 +307,8 @@ public class SelfServlet extends HttpServlet {
             contextConfig.load(fis);
         } catch (IOException e) {
             e.printStackTrace();
-        }finally{
-            if(null!=fis){
+        } finally {
+            if (null != fis) {
                 try {
                     fis.close();
                 } catch (IOException e) {
@@ -299,12 +318,32 @@ public class SelfServlet extends HttpServlet {
         }
     }
 
+    /**
+     * 将首字母小写
+     *
+     * @param simpleName
+     * @return
+     */
     private String toLowerFirstCase(String simpleName) {
-        char [] chars = simpleName.toCharArray();
+        char[] chars = simpleName.toCharArray();
         //之所以加，是因为大小写字母的ASCII码相差32，
         // 而且大写字母的ASCII码要小于小写字母的ASCII码
         //在Java中，对char做算学运算，实际上就是对ASCII码做算学运算
         chars[0] += 32;
         return String.valueOf(chars);
+    }
+
+    /**
+     * url传过来的参数都是string类型的
+     *
+     * @param type
+     * @param value
+     * @return
+     */
+    private Object convert(Class<?> type, String value) {
+        if (Integer.class == type) {
+            return Integer.valueOf(value);
+        }
+        return value;
     }
 }
