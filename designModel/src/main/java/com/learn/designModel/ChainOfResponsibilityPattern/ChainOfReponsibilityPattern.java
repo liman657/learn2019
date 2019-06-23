28a26b75229513fe1553336967f259ff1e867e7d
liman657 <657271181@qq.com>
2019-06-23 17:08:56 +0800

    责任链模式

--- designModel/src/main/java/com/learn/designModel/ChainOfResponsibilityPattern/ChainOfReponsibilityPattern.java
+++ designModel/src/main/java/com/learn/designModel/ChainOfResponsibilityPattern/ChainOfReponsibilityPattern.java
@@ -9,18 +9,29 @@ public class ChainOfReponsibilityPattern {
 
     private IRequestProcessor requestProcessor;
 
+    /**
+     * 真个处理链的构造
+     */
     public void setUp(){
+        //逆序构造处理链
         PrintProcessor printProcessor = new PrintProcessor();
         SaveProcessor saveProcessor = new SaveProcessor(printProcessor);
         CheckProcessor checkProcessor = new CheckProcessor(saveProcessor);
         requestProcessor = checkProcessor;
     }
 
+    /**
+     * 处理客户端的请求
+     * @param request
+     */
     public void print(Request request){
         requestProcessor.processor(request);
     }
 
-
+    /**
+     * 测试方法
+     * @param args
+     */
     public static void main(String[] args) {
         Request request = new Request();
         request.setName("this is my data");
@@ -28,7 +39,6 @@ public class ChainOfReponsibilityPattern {
         ChainOfReponsibilityPattern dealRequest = new ChainOfReponsibilityPattern();
         dealRequest.setUp();
         dealRequest.print(request);
-
     }
 
 }
