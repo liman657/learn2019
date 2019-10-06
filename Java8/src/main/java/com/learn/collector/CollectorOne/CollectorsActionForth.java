2f1a96eeef44101c74fb953d206ffd60f82ca30b
liman657 <657271181@qq.com>
2019-10-06 19:08:59 +0800

    日常提交

--- Java8/src/main/java/com/learn/collector/CollectorOne/CollectorsActionForth.java
+++ Java8/src/main/java/com/learn/collector/CollectorOne/CollectorsActionForth.java
@@ -2,11 +2,15 @@ package com.learn.collector.CollectorOne;
 
 import com.learn.collector.common.Dish;
 import com.learn.collector.common.DishContainer;
+import sun.plugin.javascript.navig.LinkArray;
 
 import javax.swing.plaf.synth.SynthTextAreaUI;
 import java.util.DoubleSummaryStatistics;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.stream.Collectors;
 
 /**
@@ -21,8 +25,15 @@ public class CollectorsActionForth {
 
     public static void main(String[] args) {
         testSummingDouble();
+        testSummingInt();
+        testToCollections();
+        testToConcurrentMap();
+        testToConcurrentHashMapWithBinaryOperator();
     }
 
+    /**
+     * summingDouble
+     */
     private static void testSummingDouble() {
         System.out.println("testSummingDouble");
         Optional.of(dishes.stream().collect(Collectors.summarizingDouble(Dish::getCalories)))
@@ -31,6 +42,40 @@ public class CollectorsActionForth {
         Optional.of(dishes.stream().map(Dish::getCalories)
                 .mapToInt(Integer::intValue).sum())
                 .ifPresent(System.out::println);
+    }
+
+    private static void testSummingInt(){
+        System.out.println("testSummingInt");
+        Optional.of(dishes.stream().collect(Collectors.summarizingInt(Dish::getCalories)))
+                .ifPresent(System.out::println);
+    }
+
+    /**
+     * toCollections，将stream的数据放到自己指定的集合类型中
+     */
+    private static void testToCollections(){
+        System.out.println("testToCollections");
+        Optional.ofNullable(dishes.stream().filter(d->d.getCalories()>500).collect(Collectors.toCollection(LinkedList::new)))
+                .ifPresent(System.out::println);
+    }
+
+    /**
+     * 将stream的数据转换成从currentHashMap
+     */
+    private static void testToConcurrentMap(){
+        System.out.println("testToConcurrentMap");
+        Optional.ofNullable(dishes.stream().collect(Collectors.toConcurrentMap(Dish::getName,Dish::getCalories)))
+                .ifPresent(v->{
+                    System.out.println(v);
+                    System.out.println(v.getClass());
+                });
+    }
+
+    private static void testToConcurrentHashMapWithBinaryOperator(){
+        System.out.println("testToConcurrentHashMapWithBinaryOperator");
+        Optional.ofNullable(dishes.stream().collect(
+                Collectors.toConcurrentMap(Dish::getType,v->1L,(a,b)->a+b,ConcurrentSkipListMap::new)
+        )).ifPresent(System.out::println);
     }
 
 }
--- Java_netty/src/main/java/com/learn/netty/NIOServer.java
+++ Java_netty/src/main/java/com/learn/netty/NIOServer.java
@@ -13,10 +13,9 @@ import java.util.Iterator;
  * NIO 即Non-block IO，非阻塞同步通信方式
  * 从BIO的阻塞到NIO的非阻塞，这是一大进步，归功于Buffer，Channel，Selector三个设计实现
  * Buffer：缓冲区。NIO的数据操作都是在缓冲区中进行。
- *         缓冲区实际上是一个数组，而BIO是将数据直接写入或读取到stream对象。
+ * 缓冲区实际上是一个数组，而BIO是将数据直接写入或读取到stream对象。
  * Channel：通道。NIO可以通过Channel进行数据的读，写和同时读写操作。
  * Selector：多路复用器。NIO编程的基础。多路复用器提供选择已经就绪状态任务的能力。
- *
  */
 public class NIOServer implements Runnable {
     private final int BUFFER_SIZE = 1024; // 缓冲区大小
@@ -43,8 +42,8 @@ public class NIOServer implements Runnable {
              * SelectionKey.OP_CONNECT : 表示关注socket channel的连接完成事件
              * SelectionKey.OP_ACCEPT : 表示关注server-socket channel的accept事件
              */
-            channel.register(selector,SelectionKey.OP_ACCEPT);
-            System.out.println("Server start >>>>>>>> port : "+PORT);
+            channel.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("Server start >>>>>>>> port : " + PORT);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -52,28 +51,28 @@ public class NIOServer implements Runnable {
 
     // 需要一个线程负责Selector的轮询
     public void run() {
-        while(true){
+        while (true) {
             try {
                 //1.多路复用监听器阻塞
                 selector.select();
                 //2.多路复用器已经选择结果集
                 Iterator<SelectionKey> selectionKeys = selector.selectedKeys().iterator();
                 //3.不停的轮询
-                while(selectionKeys.hasNext()){
+                while (selectionKeys.hasNext()) {
                     //4.获取其中一个key
                     SelectionKey key = selectionKeys.next();
                     //5.获取后从容器中移出
                     selectionKeys.remove();
                     //6.只获取有效的key
-                    if(!key.isValid()){
+                    if (!key.isValid()) {
                         continue;
                     }
                     //阻塞状态处理
-                    if(key.isAcceptable()){
+                    if (key.isAcceptable()) {
                         accept(key);
                     }
                     //可读状态处理
-                    if(key.isReadable()){
+                    if (key.isReadable()) {
                         read(key);
                     }
                 }
@@ -87,9 +86,10 @@ public class NIOServer implements Runnable {
      * 设置阻塞，等待client请求。
      * 在传统IO中，用的是ServerSocket和Socket。
      * 在NIO中用的是在NIO中采用的ServerSocketChannel和SocketChannel。
+     *
      * @param selectionKey
      */
-    private void accept(SelectionKey selectionKey){
+    private void accept(SelectionKey selectionKey) {
 
         try {
             //1.获取通道服务
@@ -99,21 +99,44 @@ public class NIOServer implements Runnable {
             //3.设置服务器通道为非阻塞模式，false为非阻塞模式
             socketChannel.configureBlocking(false);
             //4.把通道注册到多路复用器上，并设置读取标识
-            socketChannel.register(selector,SelectionKey.OP_READ);
+            socketChannel.register(selector, SelectionKey.OP_READ);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     /**
-     *
      * @param selectionKey
      */
-    private void read(SelectionKey selectionKey){
-        readBuffer.clear();
+    private void read(SelectionKey selectionKey) {
+        try {
+            //1.清空缓冲区数据
+            readBuffer.clear();
+            //2.获取在多路复用器上注册的通道
+            SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+            //3.读取数据，返回
+            int count = socketChannel.read(readBuffer);
+            //4.返回内容为-1，表示没有数据
+            if(-1 == count){
+                selectionKey.channel().close();
+                selectionKey.cancel();
+                return;
+            }
+            //5.有数据则在读取数据前进行复位操作
+            readBuffer.flip();
+            //6.根据缓冲区大小创建一个相应大小的bytes数组，用来获取值
+            byte[] bytes = new byte[readBuffer.remaining()];
+            //7.接受缓冲区数据
+            readBuffer.get(bytes);
+            //8.打印获取到的数据
+            System.out.println("NIO Server : "+new String(bytes,"UTF-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public static void main(String[] args) {
-
+        new Thread(new NIOServer()).start();
     }
 }
