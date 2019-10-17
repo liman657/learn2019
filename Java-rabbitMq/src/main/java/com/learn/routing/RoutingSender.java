18781fbfcd6aa5f0d8f880e6eca64dd6f1c0d23f
liman657 <657271181@qq.com>
2019-10-17 19:13:22 +0800

    日常提交

---
+++ Java-rabbitMq/src/main/java/com/learn/routing/RoutingSender.java
@@ -0,0 +1,49 @@
+package com.learn.routing;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * autor:liman
+ * createtime:2019/10/10
+ * comment:用于说明routingkey的消息发送者
+ */
+public class RoutingSender {
+
+    private static final String EXCHANGE_NAME="logs";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+        //connection与MQ主机获得连接
+        Connection connection = connectionFactory.newConnection();
+
+        Channel channel = connection.createChannel();
+        channel.exchangeDeclare(EXCHANGE_NAME,"direct");//声明队列的时候指定type
+
+        String queueName = "";
+        String message = "";
+        String routeKey = "";
+        for(int i = 0;i<100;i++){
+
+            if(i%3==0){
+                queueName = "black_queue";
+            }else{
+                queueName = "orange_queue";
+            }
+            message = "this message color is ["+i+"]"+queueName;
+            routeKey = queueName+"Key";
+            System.out.println("开始发送消息："+message);
+
+            //发送消息的时候指定routeKey
+            channel.basicPublish(EXCHANGE_NAME,routeKey,null,message.getBytes("UTF-8"));
+        }
+
+    }
+
+}
---
+++ Java-rabbitMq/src/main/java/com/learn/routing/RoutingWorkerOrange.java
@@ -0,0 +1,50 @@
+package com.learn.routing;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * autor:liman
+ * createtime:2019/10/10
+ * comment: 订阅Orange类消息的消费者
+ */
+public class RoutingWorkerOrange {
+
+    private static final String QUEUE_NAME = "orange_queue";
+    private static final String EXCHANGE_NAME = "logs";
+
+    private static int count = 0;
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+
+        Connection connection = connectionFactory.newConnection();
+
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME,"direct");
+        channel.queueDeclare(QUEUE_NAME,false,false,false,null);
+//        String queueName = channel.queueDeclare().getQueue();
+        channel.queueBind(QUEUE_NAME,EXCHANGE_NAME,QUEUE_NAME+"Key");
+
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received ["+(++count)+"]'" + message + "'");
+        };
+
+        //手动确认消息机制是默认开启的，但是设置了自动确认之后，手动确认被关闭
+        boolean autoAck = true;
+        //正式消费消息，指定消费消息的回调内部类
+        channel.basicConsume(QUEUE_NAME, autoAck, deliverCallback, consumerTag -> { });
+    }
+
+
+}
