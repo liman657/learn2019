5816b4b56575334f721555c79e9f22acb241a784
liman657 <657271181@qq.com>
2020-03-22 10:57:37 +0800

    [20190711-condition,countDownLatch,cyclicbarrier实例]

--- dataStructure/src/main/java/com/learn/graph/ListNDG.java
+++ dataStructure/src/main/java/com/learn/graph/ListNDG.java
@@ -1,6 +1,6 @@
 package com.learn.graph;
 
-import com.learn.LinkList.ListNode;
+import java.util.LinkedList;
 
 /**
  * autor:liman
@@ -12,7 +12,7 @@ public class ListNDG {
     Vertex[] vertexList;
     int size;
 
-    class Vertex{
+    class Vertex {
         char ch;
         Vertex next;
 
@@ -20,25 +20,25 @@ public class ListNDG {
             this.ch = ch;
         }
 
-        void add(char ch){
+        void add(char ch) {
             Vertex node = this;
-            while(node.next!=null){//找到链表结尾
+            while (node.next != null) {//找到链表结尾
                 node = node.next;
             }
             node.next = new Vertex(ch);
         }
     }
 
-    public ListNDG(char[] vertexs,char[][] edgs){
+    public ListNDG(char[] vertexs, char[][] edgs) {
         size = vertexs.length;
         this.vertexList = new Vertex[size];//确定邻接表的大小
         //设置邻接表的每一个节点
-        for(int i = 0;i<size;i++){
+        for (int i = 0; i < size; i++) {
             this.vertexList[i] = new Vertex(vertexs[i]);
         }
 
         //存储边的信息
-        for(char[] c:edgs){
+        for (char[] c : edgs) {
             int p1 = getPosition(c[0]);
             vertexList[p1].add(c[1]);
             int p2 = getPosition(c[1]);
@@ -47,25 +47,82 @@ public class ListNDG {
     }
 
     //根据顶点名称获取链表下标
-    private int getPosition(char ch){
-        for(int i =0;i<size;i++){
-            if(vertexList[i].ch==ch){
+    private int getPosition(char ch) {
+        for (int i = 0; i < size; i++) {
+            if (vertexList[i].ch == ch) {
                 return i;
             }
         }
         return -1;
     }
 
-    public void print(){
-        for(int i = 0;i<size;i++){
+    public void print() {
+        for (int i = 0; i < size; i++) {
             Vertex temp = vertexList[i];
-            while(temp!=null){
-                System.out.print(temp.ch+" ");
+            while (temp != null) {
+                System.out.print(temp.ch + " ");
                 temp = temp.next;
             }
             System.out.println();
         }
     }
+
+    public void DFS() {
+        boolean[] beTraversed = new boolean[size];
+//        beTraversed[getPosition(vertexList[0].ch)] = true;
+        DFS(beTraversed, vertexList[0]);
+    }
+
+    /**
+     * 深度优先遍历
+     *
+     * @param beTraversed
+     * @param temp
+     */
+    public void DFS(boolean[] beTraversed, Vertex temp) {
+        System.out.print(temp.ch + " ");
+        beTraversed[getPosition(temp.ch)] = true;
+        while (temp != null) {
+            if (beTraversed[getPosition(temp.ch)] == false) {
+                DFS(beTraversed, vertexList[getPosition(temp.ch)]);
+            }
+            temp = temp.next;
+        }
+    }
+
+    public void BFS(){
+        boolean[] beTraversed = new boolean[size];
+        beTraversed[getPosition(vertexList[0].ch)] = true;
+        System.out.print(vertexList[0].ch+" ");
+        int startPos = getPosition(vertexList[0].ch);
+        BFS(startPos,beTraversed);
+    }
+
+    /**
+     * 广度优先搜素遍历
+     * @param beTraversed
+     */
+    public void BFS(int x,boolean[] beTraversed){
+        Vertex temp = vertexList[x];
+        LinkedList<Vertex> list = new LinkedList<>();
+        while(temp!=null){
+            if(beTraversed[getPosition(temp.ch)] == false){
+                System.out.print(temp.ch+" ");
+                beTraversed[getPosition(temp.ch)] = true;
+                list.add(vertexList[getPosition(temp.ch)]);
+            }
+            temp = temp.next;
+        }
+
+        while(!list.isEmpty()){
+            Vertex node = list.pop();
+            int pos = getPosition(node.ch);
+            BFS(pos,beTraversed);
+        }
+    }
+
+
+
 
     public static void main(String[] args) {
         char[] vexs = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'};
@@ -82,8 +139,11 @@ public class ListNDG {
                 {'E', 'H'},
                 {'H', 'K'}};
 
-        ListNDG listNDG = new ListNDG(vexs,edges);
+        ListNDG listNDG = new ListNDG(vexs, edges);
         listNDG.print();
+        listNDG.DFS();
+        System.out.println();
+        listNDG.BFS();
     }
 
 }
